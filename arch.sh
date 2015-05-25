d3f3d480484383df719729b320d89e31bff2de15
kent <kent@mail.norbelbaby.com.tw>
2015-05-25 15:45:00 +0800

    修改script與pom

--- arch.sh
+++ arch.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-PWD=`pwd`
+CD=`pwd`
 cd ./springJdbi
 cp pom.xml src/test/resources/
-mvn clean wro4j:run archetype:create-from-project -Darchetype.properties=../archetype.properties
-mv ./target/generated-sources/archetype/src/main/resources/archetype-resources/src/test/resources/pom.xml ./target/generated-sources/archetype/src/main/resources/archetype-resources/
-sed -i s/springJdbi-archetype/springJdbiArch/g ./target/generated-sources/archetype/pom.xml
-rm src/test/resources/pom.xml
-rm -rf ./src/main/webapp/wro/
-cd ./target/generated-sources/archetype
+mvn clean wro4j:run archetype:create-from-project -Darchetype.properties=../archetype.properties && \
+mv ./target/generated-sources/archetype/src/main/resources/archetype-resources/src/test/resources/pom.xml ./target/generated-sources/archetype/src/main/resources/archetype-resources/ && \
+sed -i s/springJdbi-archetype/springJdbiArch/g ./target/generated-sources/archetype/pom.xml && \
+sed -i '$ d' ./target/generated-sources/archetype/pom.xml && \
+cat $CD/profiles.xml >> ./target/generated-sources/archetype/pom.xml && \
+echo -e "\n</project>" >> ./target/generated-sources/archetype/pom.xml && \
+cd ./target/generated-sources/archetype && \
 mvn clean install archetype:integration-test
-cd $PWD
+rm $CD/springJdbi/src/test/resources/pom.xml
+rm -rf $CD/springJdbi/src/main/webapp/wro/
+cd $CD
