ff713684c349b646540426175cc0b933473b7a23
Kent Yeh <kent@mail.norbelbaby.com.tw>
2016-06-08 17:34:24 +0800

    add spring session

---
+++ springJdbi/src/main/java/com/github/kentyeh/context/ObjectStreamSerializer.java
@@ -0,0 +1,41 @@
+package com.github.kentyeh.context;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.StreamSerializer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class ObjectStreamSerializer implements StreamSerializer<Object> {
+
+    @Override
+    public int getTypeId() {
+        return 2;
+    }
+
+    @Override
+    public void write(ObjectDataOutput objectDataOutput, Object object)
+            throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream((OutputStream) objectDataOutput);
+        out.writeObject(object);
+        out.flush();
+    }
+
+    @Override
+    public Object read(ObjectDataInput objectDataInput) throws IOException {
+        ObjectInputStream in = new ObjectInputStream((InputStream) objectDataInput);
+        try {
+            return in.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
---
+++ springJdbi/src/main/java/com/github/kentyeh/context/SpringSessionInitializer.java
@@ -0,0 +1,13 @@
+package com.github.kentyeh.context;
+/**
+ *
+ * @author Kent Yeh
+ */
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class SpringSessionInitializer extends AbstractHttpSessionApplicationInitializer {
+
+    public SpringSessionInitializer() {
+    }
+
+}
