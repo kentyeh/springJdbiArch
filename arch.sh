388a04e27ccc19db7dd4a60a10aadf6817ebb1ae
kentyeh <kent.yeh2000@gmail.com>
2016-06-09 22:06:53 +0800

    update versions

--- arch.sh
+++ arch.sh
@@ -7,6 +7,7 @@ mv ./target/generated-sources/archetype/src/main/resources/archetype-resources/s
 cp ./src/site/site.xml ./target/generated-sources/archetype/src/main/resources/archetype-resources/src/site/ && \
 sed -i s/springJdbi-archetype/springJdbiArch/g ./target/generated-sources/archetype/pom.xml && \
 sed -i -E '0,/<version>[0-9]+\.[0-9]+\.[0-9]*<\/version>/s//<version>1.0-SNAPSHOT<\/version>/' ./target/generated-sources/archetype/src/main/resources/archetype-resources/pom.xml &&\
+sed -i s/\$\{artifactId\}Arch/\$\{artifactId\}/ ./target/generated-sources/archetype/src/main/resources/archetype-resources/pom.xml &&\
 find ./target/generated-sources/archetype/src -name "*.java" -o -name "*.xml"|xargs sed -i s/$\{groupId\}/$\{package\}/g && \
 sed -i '$ d' ./target/generated-sources/archetype/pom.xml && \
 cat $CD/profiles.xml >> ./target/generated-sources/archetype/pom.xml && \
